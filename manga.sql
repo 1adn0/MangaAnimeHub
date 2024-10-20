3930bc8d556af7269eba7c6ec943013f0cda12f3
1adn0 <120452870+1adn0@users.noreply.github.com>
2024-10-20 14:37:31 +0300

    Delete manga.sql

--- manga.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE manga (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(100),
-    genre VARCHAR(50),
-    status ENUM('completed', 'ongoing') NOT NULL,
-    rating DECIMAL(3, 2) DEFAULT 0,
-    description TEXT
-);
