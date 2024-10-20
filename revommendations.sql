668e193b219ed0e596e4d6bd8ac65ae85b8be73f
1adn0 <120452870+1adn0@users.noreply.github.com>
2024-10-20 14:38:23 +0300

    Delete revommendations.sql

--- revommendations.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE recommendations (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    recommended_item_id INT NOT NULL,  -- Может быть манга или аниме
-    reason TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
