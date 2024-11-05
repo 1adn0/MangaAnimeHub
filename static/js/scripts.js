76a8c5cf2e86039309fb52a162061bfac4b596fd
1adn0 <120452870+1adn0@users.noreply.github.com>
2024-11-05 22:32:12 +0300

    pon

---
+++ static/js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const exploreButton = document.getElementById("exploreButton");
+    exploreButton.addEventListener("click", () => {
+        alert("Добро пожаловать в MangaAnimeHub! Начните исследовать любимые аниме и мангу.");
+    });
+});
